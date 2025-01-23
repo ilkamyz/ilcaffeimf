c5fed38a7ed1ccc830aef4ef332acd0176bc8b52
Luca <75177548+Lucacarrino@users.noreply.github.com>
2025-01-23 20:16:33 +0100

    Update urls.py
    aggiunta url home.html

--- Giornalino/giornalino/articoli/urls.py
+++ Giornalino/giornalino/articoli/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.lista_articoli, name='lista_articoli'),
     path('<int:pk>/', views.dettaglio_articolo, name='dettaglio_articolo'),
     path('mi-piace/<int:pk>/', views.toggle_mi_piace, name='toggle_mi_piace'),
-    path('autore/<str:author_name>/', views.autore_dettaglio, name = 'autore_dettaglio')
+    path('autore/<str:author_name>/', views.autore_dettaglio, name = 'autore_dettaglio'),
+    path("home/", views.home, name="home"),
 ]
