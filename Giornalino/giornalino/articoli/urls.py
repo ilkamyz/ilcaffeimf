b124f65c5b998ab840f4557299493ed20e69d6df
Luca <75177548+Lucacarrino@users.noreply.github.com>
2025-01-27 19:22:07 +0100

    Update urls.py
    cambio url

--- Giornalino/giornalino/articoli/urls.py
+++ Giornalino/giornalino/articoli/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.lista_articoli, name='lista_articoli'),
-    path('<int:pk>/', views.dettaglio_articolo, name='dettaglio_articolo'),
-    path('mi-piace/<int:pk>/', views.toggle_mi_piace, name='toggle_mi_piace'),
-    path('autore/<str:author_name>/', views.autore_dettaglio, name = 'autore_dettaglio'),
-    path("home/", views.home, name="home"),
+    path("articoli/", views.lista_articoli, name="lista_articoli"),
+    path("<int:pk>/", views.dettaglio_articolo, name="dettaglio_articolo"),
+    path("mi-piace/<int:pk>/", views.toggle_mi_piace, name="toggle_mi_piace"),
+    path("autore/<str:author_name>/", views.autore_dettaglio, name="autore_dettaglio"),
+    path("", views.home, name="home"),
 ]
