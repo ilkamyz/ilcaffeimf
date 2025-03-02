03c70a05838f8cd151b806c389f451f3ce426c2b
Luca <75177548+Lucacarrino@users.noreply.github.com>
2025-03-03 00:18:18 +0100

    Delete account directory

--- account/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from django.contrib.auth.hashers import make_password
-from .models import  AccountSubscriber
-
-
-
-
--- account/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-
-urlpatterns = [
-    path("login/", views.login, name="login"),
-    path("registrati/", views.registrati, name="registrati"),
-    path("logout/", views.logout, name="logout"),
-]
