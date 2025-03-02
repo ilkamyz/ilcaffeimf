40a0c02c73bbd842b522ca11161e8659e7c91cbe
Luca <75177548+Lucacarrino@users.noreply.github.com>
2025-03-03 00:19:00 +0100

    Delete newsletter directory

--- newsletter/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class IscrizioneNewsletter(models.Model):
-    email = models.EmailField(unique=True)
-    data_iscrizione = models.DateTimeField(auto_now_add=True)
-    is_active = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.email
